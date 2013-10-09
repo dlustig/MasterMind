da0731f57e0121007aa5d7587d518b848ebe5206
zgoodwyn <zgoodwyn@yahoo.com>
2013-10-09 12:21:41 -0400

    fixed input validation

--- Game.java
+++ Game.java
@@ -15,6 +15,7 @@ public class Game{
                     try {alphsize = in.nextInt();}
                     
                     catch(Exception e){
+                    	in.nextLine();
                     	continue;
                     }
                     
@@ -35,6 +36,7 @@ public class Game{
                 	try{numRounds = in.nextInt();}
                 	
                 	catch(Exception q){
+                		in.nextLine();
                 		continue;
                 		
                 	}
@@ -87,25 +89,4 @@ public class Game{
 
 
 }
-/*
-scrap all that
-game begins
-Board calls "getAlfSize" on code
-Board calls "getRounds" and stores the int in "numRounds"
-Board creates a code object and names it "CorrectCode"
-Board calls "inputCodeSecret" on "CorrectCode", giving it a value
-	function "inputCodeSecret" has input validation (assume validation function)
-Board sets boolean "won" to false
-Board sets "onRound" to zero
-while(won==false && onRound < numRounds)
-	accept input and put it in "GuessCode"
-	call "compareCode" on "CorrectCode" with "GuessCode" as argument
-		"compareCode" prints string of form "x correct y in puzzle"
-		"compareCode" returns a boolean which is true if "GuessCode" is the same as "CorrectCode"
-	make "won" equal to output of "compareCode"
-	increment "onRound"
-if(won==true)
-	print "Codebreaker wins!"
-else
-	print "Codemaker wins!"
-*/
+
